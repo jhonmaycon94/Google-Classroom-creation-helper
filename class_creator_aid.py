4b2c289ce964b2e22552e2777f53795f99773ec0
jhon maycon <jhonmaycon94@gmail.com>
2020-07-12 12:34:44 -0300

    almost final

--- class_creator_aid.py
+++ class_creator_aid.py
@@ -1,27 +1,21 @@
 import pyperclip, re, unicodedata, time
-from pynput.keyboard import Listener
 
-tb_alunos = pyperclip.paste()
+op = ""
 
-alunos = re.findall(r"(\b[A-Z][^\d]{1,}\t\t)", tb_alunos)
+while op != 's':
+    tb_alunos = pyperclip.paste()
 
-for aluno in alunos:
-    aluno = unicodedata.normalize("NFKD", aluno.strip()).encode("ascii", "ignore").decode("ascii")
-
-i = 0
-
-def on_press(key):
-    global i
-    try:
-        if key == 'p':
-            pyperclip.copy(alunos[i].strip())
-            if i < len(alunos):
-                i=i+1
-    except AttributeError:
-        pass     
-
-def on_release(key):
-    pass
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+    alunos = re.findall(r"(\b[A-Z][^\d]{1,}\t\t)", tb_alunos)
+    
+    qnt = 0
+    for aluno in alunos:
+        aluno = unicodedata.normalize("NFKD", aluno.strip()).encode("ascii", "ignore").decode("ascii")
+        pyperclip.copy(aluno)
+        qnt = qnt + 1
+        print("na área de transferencia: " + aluno +"\n")
+        input("tecle enter para o próximo nome...\n")
+        print(str(qnt) + " aluno(s) copiado(s) \n")
+        
+    print("Turma Finalizada: "+ str(qnt) + " copiados\n")    
+    op = input("tecle s para sair ou enter para colar nova turma\n")    
 
