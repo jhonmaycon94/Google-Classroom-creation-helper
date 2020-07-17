126a101366d66775e96f873b4bb953a501a35276
jhon maycon <jhonmaycon94@gmail.com>
2020-07-17 16:22:18 -0300

    refactoring

--- class_creator_aid.py
+++ class_creator_aid.py
@@ -1,11 +1,4 @@
-import pyperclip, re, unicodedata, time, openpyxl
-
-wb = openpyxl.load_workbook("emails academicos.xlsx")
-ws_alunos = wb['Planilha1']
-alunos_from_ws = []
-
-for i in range(1, ws_alunos.max_row):
-    alunos_from_ws.append(ws_alunos.cell(row=i, column=1).value)
+import pyperclip, re, unicodedata, openpyxl
 
 def normatizar(alunos):
     alunos_formatados = []
@@ -13,43 +6,64 @@ def normatizar(alunos):
         aluno = unicodedata.normalize("NFKD", aluno.strip()).encode("ascii", "ignore").decode("ascii")
         aluno = aluno.upper().strip()
         alunos_formatados.append(aluno)
-
     return alunos_formatados
 
-alunos_from_ws = normatizar(alunos_from_ws)
+def extract_students_from_copy(copy):
+    alunos = re.findall(r"(\b[A-Z][^\d]{1,}\t\t)", copy)
+    alunos = normatizar(alunos)
+    if not alunos:
+        copy = str(copy).split("\n")
+        copy = normatizar(copy)
+        copy = "\n".join(copy)
+        alunos = re.findall(r"((?<=\d\t)[A-Z ]{3,})", copy)
+    return alunos  
 
-tb_alunos = pyperclip.paste()
+def load_students_from_sheet(sheet):
+    alunos = []
+    for i in range(1, sheet.max_row):
+        alunos.append(sheet.cell(row=i, column=1).value)
+    return alunos
 
-alunos_unicode = re.findall(r"(\b[A-Z][^\d]{1,}\t\t)", tb_alunos)
-if not alunos_unicode:
-    tb_alunos = str(tb_alunos).split("\n")
-    tb_alunos = normatizar(tb_alunos)
-    tb_alunos = "\n".join(tb_alunos)
-    alunos_unicode = re.findall(r"((?<=\d\t)[A-Z ]{3,})", tb_alunos)
+def search_student_in_sheet(copied_students, alunos_from_sheet):
+    alunos = []
+    for aluno in copied_students:
+        match = [aluno_ws for aluno_ws in alunos_from_sheet if aluno in aluno_ws]
+        if not match:
+            continue
+        alunos.append(match)
+    return alunos
 
-alunos = normatizar(alunos_unicode) 
+def alunos_without_email(alunos):
+    alunos = []
+    for aluno in alunos:
+        if "@" not in aluno:
+            alunos.append(aluno)
+    return alunos
 
-final_list = []
-alunos_sem_email = []
+def alunos_to_clipboard(alunos):
+    pyperclip.copy("\n".join(alunos))
 
-qtd = 0
+def format_output(alunos, alunos_sem_email):
+    qnt = len(alunos)
+    print(str(qnt)+" Alunos copiados para área de transferencia\n")
+    if alunos_sem_email: 
+        print("ALUNOS SEM EMAIL:\n")
+        for aluno in alunos_sem_email:
+            print(aluno)
 
-for aluno in alunos:
-    match = [aluno_ws for aluno_ws in alunos_from_ws if aluno in aluno_ws]
-    if not match:
-        continue 
-    qtd = qtd + 1
-    match = "\n".join(match)
-    final_list.append(str(qtd)+" "+match)
-    if '@' not in match:
-        alunos_sem_email.append(match)
-    
-pyperclip.copy("\n".join(final_list))
+wb = openpyxl.load_workbook("emails academicos.xlsx")
+ws_alunos = wb['Planilha1']
+alunos_from_ws = load_students_from_sheet(ws_alunos)
+alunos_from_ws = normatizar(alunos_from_ws)    
 
-print(str(qtd)+" Alunos copiados para área de transferencia\n")
-if alunos_sem_email:
-    print("ALUNOS SEM EMAIL:\n")
-    for aluno in alunos_sem_email:
-        print(aluno)
+copied_students = pyperclip.paste()
+alunos = extract_students_from_copy(copied_students)
+
+alunos_para_copiar = search_student_in_sheet(alunos, alunos_from_ws)
+alunos_sem_email = alunos_without_email(alunos_to_clipboard)
+    for aluno 
+#alunos_to_clipboard(alunos_para_copiar)
+format_output(alunos_para_copiar, alunos_sem_email)
+
 
 
